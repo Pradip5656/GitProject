d6f4f20961c7765043729aac9f2dd85b2f690e15
pradipmane <pradipmaane@gmail.com>
2021-11-23 15:23:11 +0530

    i have created the project.

---
+++ src/javaGitclass.java
@@ -0,0 +1,12 @@
+
+public class javaGitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi Git Project");
+System.out.println("Done by lead");
+System.out.println("Hi Guys");
+
+	}
+
+}
